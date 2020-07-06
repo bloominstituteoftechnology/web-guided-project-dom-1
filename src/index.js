5cb2978eff778e04d84984a78cd34d4c765dd642
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-06 16:09:44 +0200

    prep for class

--- src/index.js
+++ src/index.js
@@ -1,61 +1,17 @@
-// See this in the console to make sure the project is wired up correctly
-console.log('The project is UP')
+// USING THE DOM TO EDIT ELEMENTS ON THE PAGE (AND ADD NEW ONES)
 
-// STEP 1- console.dir document and study it
+// 1- Finding an element on the page and saving a reference to it
+//  1a- querySelector
+//  1b- querySelectorAll
 
-// STEP 2- Grab the Lambda Cat heading in the console. Access the following:
-//  - the .id
-//  - the .classList
-//  - the data inside a data-attribute using .dataset
-//  - the text content using .textContent
-//  - any attribute using .getAttribute('id') // example using id
+// 2- Changing an element's text content
 
-// STEP 3- find the body of the document in the DOM and find:
-//  - Its parentElement
-//  - Its firstElementChild
-//  - Its firstElementChild's nextSibling element
+// 3- Changing any property
+//  3a- with dot notation
+//  3b- setAttribute
 
-// STEP 4- find the Lambda Cat heading with firstElementChild and nextSibling
+// 4- Changing the style
+//  4a- by changing class names on the element
+//  4b- by manipulating inline styles on the element
 
-// STEP 5- find the children of the nav, convert it into a real array
-
-// STEP 6- NO CSS SELECTOR - find the following elements or groups of elements:
-//  - all the divs in the page
-//  - all elements with a className of 'card'
-//  - the element with an id of 'mainNav'
-const allLinks = document.getElementsByTagName('a') // [COLLECTION]
-const allCards = document.getElementsByClassName('card') // [COLLECTION]
-const logoTitleOld = document.getElementById('logo-title') // [SINGLE ELEMENT]
-
-// STEP 7- do the same, but using CSS selectors
-const logoTitle = document.querySelector('#logo-title') // [SINGLE ELEMENT]
-const anchorTagsNav = document.querySelectorAll('nav a') // [COLLECTION]
-
-const nav = document.querySelector('nav') // [SINGLE ELEMENT]
-const linksfromNav = nav.querySelectorAll('a') // [COLLECTION]
-
-// STEP 8- use setAttribute to change an attribute of an element
-
-// STEP 9- use style to change inline styles of an element
-//  - make the first h3 subheading be a different color
-//  - make all the h3 subheadings be a different color using a loop
-
-// STEP 10- use classList to add, remove and toggle a class name
-//  - remove the 'extra-stuff' class name from the last paragraph
-//  - add it to the first paragraph
-//  - toggle it in the second paragraph
-
-// STEP 11- get a collection of h2s and loop over them setting text content from cardTitles
-const cardTitles = {
-  cardTitle1: 'Cats Are the Supreme Rulers',
-  cardTitle2: 'Cats Rule Dogs Drool',
-  cardTitle3: 'Give Me All the Belly Rubs',
-  cardTitle4: 'I Haz CheeseBurger',
-}
-
-// STEP 12- create a new link and append it to the main nav
-const newLinkProps = {
-  href: '#',
-  textContent: 'Site Map',
-  className: 'menu-item',
-}
+// 5- Creating new elements and appending them
