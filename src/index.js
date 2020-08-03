509fbaeb37517e26cca3f1a5dee97c4a5f35af02
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-03 14:31:20 +0200

    instructions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 // USING THE DOM TO EDIT ELEMENTS ON THE PAGE (AND ADD NEW ONES)
 
 // 1- Finding an element on the page and saving a reference to it
-//  1a- querySelector
-//  1b- querySelectorAll
+//  getElementById, getElementsByTagName, getElementsByClassName
+//  querySelector
+//  querySelectorAll
 
 // 2- Changing an element's text content
+//  textContent, innerText
 
 // 3- Changing any property
-//  3a- with dot notation
-//  3b- setAttribute
+//  with dot notation
+//  setAttribute, getAttribute
 
 // 4- Changing the style
-//  4a- by changing class names on the element
-//  4b- by manipulating inline styles on the element
+//  by changing the class names on the element
+//  by manipulating inline styles on the element
 
 // 5- Creating new elements and appending them
+
+// 6- Removing an existing element and putting it back
+
+// 7- Making a copy of the card and appending it to the card group
