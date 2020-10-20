5982b2d0cd5984378a8bf9464f0c4ad5055ac928
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-20 14:43:22 +0200

    update steps

--- src/index.js
+++ src/index.js
@@ -1,31 +1,54 @@
-// USING THE DOM TO EDIT ELEMENTS ON THE PAGE (AND ADD NEW ONES)
-
-// 1- Making a clone of the div.card ad appending it to the div.card-group
-
-// 2- Finding an element on the page and saving a reference to it
+// 👉 1- Finding an element on the page and saving a reference to it
 //  Older: getElementById, getElementsByTagName, getElementsByClassName
 //  Newer: querySelector, querySelectorAll
+//  Select the following single elements from the div.card
+
+// A- finding across the entire DOM
 const header = null
 const logoTitle = null
-const card = null
-const cardImage = null
-const cardTitle = null
-const cardSubtitle = null
-const cardText = null
-const cardLink1 = null
-const cardLink2 = null
+const firstCard = null
+// B- finding within one particular element
+const imageFirstCard = null
+const titleFirstCard = null
+const subtitleFirstCard = null
+const textFirstCard = null
+// C- traversing with dot notation
+const link1FirstCard = null
+const link2FirstCard = null
 
-// 3- Changing an element's text content
-//  textContent, innerText (subtle differences)
 
-// 4- Accessing and changing element attributes
-//  with dot notation
-//  setAttribute, getAttribute
+// 👉 2- Finding collections of elements in the DOM
+// A- Find all the anchor tags inside the nav element
+// B- Loop over the links and console.log their text content
+// C- Turn the collection of links into a real array
+// D- Use .filter to find the anchor tag with the textContent of "Home"
 
-// 5- Changing the style
-//  by changing the class names on the element
-//  by manipulating inline styles on the element
 
-// 6- Creating new elements and appending them
+// 👉 3- Changing an element's text content
+//  A- Change the cat-related content into dog-related content
+//  B- Have the students research online the difference between textContent and innerText
 
-// 7- Removing an existing element and putting it back
+
+// 👉 4- Changing any property
+//  A- Using dot notation to change a few attributes
+//  B- Using .setAttribute to change a few attributes
+
+
+// 👉 5- Changing the styling of an element
+//  A- By changing the class names on the element with the classList API
+//  B- By manipulating inline styles on the element
+
+
+// 👉 6- Creating new elements from scratch and appending them
+// Create a new link inside the nav for "Blog"
+
+
+// 👉 7- Making a copy of the card and appending it to the card group
+// DOM nodes can only exist in one spot in the DOM
+// We cannot append the same copy multiple times
+
+
+// 👉 8- Removing an existing element and putting it back [STRETCH if time allows]
+
+
+// 👉 9- Show to students the insertAdjacentHTML method [STRETCH if time allows]
