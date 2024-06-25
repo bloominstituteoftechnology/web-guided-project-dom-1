758c68c118887c89061701e57079e4437b7b95cb
Weijie XI <wxi76@berkeley.edu>
2024-06-25 13:40:02 -0700

    add content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 // 👉 1- Finding an element on the page and saving a reference to it
+// getElementById()可以到达 DOM 树中的特定标签。它将返回包含该标签的对象。
+// getElementsByTagName()方法返回一个对象集合，这个集合是一个动态的 HTMLCollection 对象。
+// getElementsByClassName()方法返回一个对象集合，这个集合是一个动态的 HTMLCollection 对象。
     //  Older: getElementById, getElementsByTagName, getElementsByClassName
     //  Newer: querySelector, querySelectorAll
     //  Select the following single elements from the div.card
@@ -8,33 +11,42 @@ const logoTitleOld = document.getElementById("logoTitle");
 
 // A- finding across the entire DOM
 // querySelector() 方法返回文档中匹配指定 CSS 选择器的一个元素。
-// querySelector() 方法仅仅返回匹配指定选择器的第一个元素。如果你需要返回所有的元素，请使用 querySelectorAll() 方法替代。
+// querySelector() 方法仅仅返回匹配指定选择器的第一个元素。
+// 如果你需要返回所有的元素，请使用 querySelectorAll() 方法替代。
 
-const header = document.querySelector("header");
+const header = document.querySelector("header");    //针对整个document，找到第一个header
 const logoTitle = document.querySelector("#logoTitle");
 const firstCard = document.querySelector(".card:nth-of-type(1)")
 
 // B- finding within one particular element
-const imageFirstCard = firstCard.querySelector("img");
+const imageFirstCard = firstCard.querySelector("img"); //在firstCard中找到第一个img
 const titleFirstCard = firstCard.querySelector("h2");
 const subtitleFirstCard = firstCard.querySelector("h3");
 const textFirstCard = firstCard.querySelector("p");
 
 // C- traversing with dot notation
-const link1FirstCard = textFirstCard.nextElementSibling;
+// The nextElementSibling property in JavaScript is used to get the next sibling element of a specified element in the DOM (Document Object Model). 
+// It returns the next sibling element that is an element node, or null if there is no such sibling.
+const link1FirstCard = textFirstCard.nextElementSibling; //找到textFirstCard的下一个兄弟节点
 const link2FirstCard = link1FirstCard.nextElementSibling;
 
 
 // 👉 2- Finding collections of elements in the DOM
 // A- Find all the anchor tags inside the nav element
+const links = document.querySelectorAll("nav a"); //找到nav下的所有a标签
 // B- Loop over the links and console.log their text content
+const linksArray = Array.from(links); //将links转换为数组
 // C- Turn the collection of links into a real array
+const textLinks = linksArray.map(link => link.textContent); //将linksArray中的每个link的textContent取出来，放到textLinks中
 // D- Use .filter to find the anchor tag with the textContent of "Home"
+const homeLink = linksArray.filter(link => link.textContent === "Home")[0]; //找到linksArray中textContent为"Home"的link
 
 
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
+titleFirstCard.textContent = "Dog Photo";
 //  B- Have the students research online the difference between textContent and innerText
+
 
 
 // 👉 4- Changing any property
